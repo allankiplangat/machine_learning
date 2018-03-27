17c461ed4b280ec2b6ef3651baadf96cf5af1bef
allankiplangat <allankiplangat22@outlook.com>
2018-03-27 19:42:39 +0300

    Encoding

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -15,3 +15,16 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 imputer = imputer.fit(feature_matrix[:, 1:3])
 feature_matrix[:, 1:3] = imputer.transform(feature_matrix[:, 1:3])
+
+# Encoding the categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_feature_matrix = LabelEncoder()
+feature_matrix[:, 0] = labelencoder_feature_matrix.fit_transform(feature_matrix[:, 0])
+
+# Dummy Encoding
+onehotencoder = OneHotEncoder(categorical_features= [0])
+feature_matrix = onehotencoder.fit_transform(feature_matrix).toarray()
+
+# Encoding the dependant variable
+labelencoder_dependant_variable = LabelEncoder()
+dependant_variable = labelencoder_dependant_variable.fit_transform(dependant_variable)
